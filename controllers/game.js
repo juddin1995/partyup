ac99e8de79900fb1f2a0e5b8d711043d5b82a1e0
jomir <jomir.uddin112@gmail.com>
2024-08-26 20:12:43 -0400

    Complete comment functionality

--- controllers/game.js
+++ controllers/game.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ensureLoggedIn = require("../middleware/ensureLoggedIn");
+const User = require("../models/user");
 
 // GET / game collection
 router.get("/", ensureLoggedIn, (req, res) => {
@@ -8,8 +9,45 @@ router.get("/", ensureLoggedIn, (req, res) => {
 });
 
 // GET /portal2
-router.get("/portal2", ensureLoggedIn, (req, res) => {
-  res.render("games/portal2.ejs");
+router.get("/portal2", ensureLoggedIn, async (req, res) => {
+  const owner = await User.findById(req.user._id);
+  res.render("games/portal2.ejs", { owner });
+});
+
+// POST /portal2 
+router.post("/portal2", ensureLoggedIn, async (req, res) => {
+  const owner = await User.findById(req.user._id);
+  owner.comments.push(req.body);
+  await owner.save();
+  res.redirect('/games/gameCollection/portal2');
+});
+
+// GET /chainedTogether
+router.get("/chainedTogether", ensureLoggedIn, async (req, res) => {
+  const owner = await User.findById(req.user._id);
+  res.render("games/chainedTogether.ejs", { owner });
+});
+
+// POST /chainedTogether
+router.post("/chainedTogether", ensureLoggedIn, async (req, res) => {
+  const owner = await User.findById(req.user._id);
+  owner.comments.push(req.body);
+  await owner.save();
+  res.redirect('/games/gameCollection/chainedTogether');
+});
+
+// GET /eldenRing
+router.get("/eldenRing", ensureLoggedIn, async (req, res) => {
+  const owner = await User.findById(req.user._id);
+  res.render("games/eldenRing.ejs", { owner });
+});
+
+// POST /eldenRing
+router.post("/eldenRing", ensureLoggedIn, async (req, res) => {
+  const owner = await User.findById(req.user._id);
+  owner.comments.push(req.body);
+  await owner.save();
+  res.redirect('/games/gameCollection/eldenRing');
 });
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema({
+  content: String,
+});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
+  comments: [commentSchema],
 });
 
 const User = mongoose.model("User", userSchema);
